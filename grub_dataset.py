8ce9ac607ebcee3748eb3e78c5bee558dfa5317f
masikol <mishasikolenko@mail.ru>
2026-01-18 09:50:59 +0300

    add data

--- grub_dataset.py
+++ grub_dataset.py
@@ -5,7 +5,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
-    with Pool(processes = 8) as pool:
-        results = pool.map(load_data, [0, -1, 5, 10, -5, -10, 15, -15])
+    with Pool(processes = 4) as pool:
+        results = pool.map(load_data, [0, -1, 10, -10])
     # end with
 # end if
