652cbd3a11df504f7b8080b95f224feaacd08ae1
masikol <mishasikolenko@mail.ru>
2026-01-19 09:30:08 +0300

    get rid if data

--- src/loader_functions/parse_liquipedia_datetime.py
+++ src/loader_functions/parse_liquipedia_datetime.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
 def parse_liquipedia_datetime(s: pd.Series) -> pd.Series:
-    s = s.str.replace(
-        r'(UTC|CET|CEST|KST|EDT|CDT|CST|EST|PDT)$',
-        '',
-        regex=True
-    ).str.strip()
+    try:
+        s = s.str.replace(
+            r'(UTC|CET|CEST|KST|EDT|CDT|CST|EST|PDT)$',
+            '',
+            regex=True
+        ).str.strip()
 
-    s = s.str.replace(r'\s*-\s*\d{1,2}:\d{2}$', '', regex=True)
+        s = s.str.replace(r'\s*-\s*\d{1,2}:\d{2}$', '', regex=True)
 
-    return pd.to_datetime(s, format='mixed', errors='coerce').dt.normalize()
+        return pd.to_datetime(s, format='mixed', errors='coerce').dt.normalize()
+    except AttributeError as e:
+        return pd.to_datetime('2020-01-01', format='mixed', errors='coerce').dt.normalize()
+    # end try
 # end def
