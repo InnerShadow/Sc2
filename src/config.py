84b01e78dd2c0a27f401e17a8243c29c792f1df3
masikol <mishasikolenko@mail.ru>
2026-01-20 10:01:52 +0300

    get B tier

--- src/config.py
+++ src/config.py
@@ -10,19 +10,19 @@ HEADERS = {
 LIQUIPEDIA_URLS = {
     'https://liquipedia.net/starcraft2/S-Tier_Tournaments' : 'S',
     'https://liquipedia.net/starcraft2/A-Tier_Tournaments' : 'A',
-    'https://liquipedia.net/starcraft2/A-Tier_Tournaments/LotV/2018-2020' : 'A',
+    # 'https://liquipedia.net/starcraft2/A-Tier_Tournaments/LotV/2018-2020' : 'A',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2025' : 'B',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2024' : 'B',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2023' : 'B',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2022' : 'B',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2021' : 'B',
     'https://liquipedia.net/starcraft2/B-Tier_Tournaments/2020' : 'B',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2025' : 'C',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2024' : 'C',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2023' : 'C',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2023' : 'C',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2021' : 'C',
-    'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2020' : 'C'
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2025' : 'C',
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2024' : 'C',
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2023' : 'C',
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2023' : 'C',
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2021' : 'C',
+    # 'https://liquipedia.net/starcraft2/C-Tier_Tournaments/2020' : 'C'
 }
 
 SAVE_PATH = os.path.join('.', 'data', 'dataset.csv')
